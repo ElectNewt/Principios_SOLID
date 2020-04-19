bd4be580e27ef02ea2633d80f9fd2e936c13302a
ElectNewt <ivanxo69@gmail.com>
2020-04-19 10:22:22 +0100

    add principio de responsabilidad unica

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Principios_SOLID
+
+Este repositorio contiene el código utiilzado en los post de NetMentor sobre los principios SOLID https://www.netmentor.es/Curso/principios-solid
---
+++ src/ResponsabilidadUnica/ArticulosServicio.cs
@@ -0,0 +1,40 @@
+﻿namespace ResponsabilidadUnica
+{
+    public class ArticulosServicio
+    {
+        private readonly Cache _cache;
+        private readonly Logging _logging;
+        private readonly Almacenamiento _almacenamiento;
+
+        public ArticulosServicio()
+        {
+            _logging = new Logging();
+            _almacenamiento = new Almacenamiento();
+            _cache = new Cache();
+        }
+
+        public void GuardarArticulo(string contenido, string titulo)
+        {
+            _logging.Info($"vamos a insertar el articulo {titulo}");
+            _almacenamiento.EscribirFichero(titulo, contenido);
+            _logging.Info($"articulo {titulo} insertado");
+            _cache.Add(titulo, contenido);
+        }
+
+        public string ConsultarArticulo(string titulo)
+        {
+            _logging.Info($"Consultar artículo {titulo}");
+
+            string contenido = _cache.Get(titulo);
+            if (!string.IsNullOrWhiteSpace(contenido))
+            {
+                _logging.Info($"Artículo encontrado en la cache {titulo}");
+                return contenido;
+            }
+
+            _logging.Info($"buscar articulo en el sistema de archivos {titulo}");
+
+            return _almacenamiento.LeerFichero(titulo);
+        }
+    }
+}
