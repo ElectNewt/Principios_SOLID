e88bbb7ba488a8863b3ec4f29acb75a2c3752dc9
ElectNewt <ivanxo69@gmail.com>
2020-05-03 22:12:02 +0100

    segregacion de interfaces

---
+++ src/SegregacionInterfaces/AlmacenamientoFichero.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace SegregacionInterfaces
+{
+    public class AlmacenamientoFichero : IAlmacenamiento
+    {
+        readonly string path = "C:/temp";
+        readonly IFicheroInformacion _ficheroInformacion;
+
+        public AlmacenamientoFichero(IFicheroInformacion ficheroInformacion)
+        {
+            _ficheroInformacion = ficheroInformacion;
+        }
+
+        public void Guardar(string titulo, string contenido)
+        {
+            File.WriteAllText($"{path}/{titulo}.txt", contenido);
+        }
+
+        public string Leer(string titulo)
+        {
+            if (!LeerFichero(titulo).Exists)
+                return null;
+
+            return File.ReadAllText($"{path}/{titulo}.txt");
+        }
+
+        private FileInfo LeerFichero(string nombre) {
+            return _ficheroInformacion.GetInformacion(nombre);
+        }
+
+    }
+}
---
+++ src/SegregacionInterfaces/DBRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegregacionInterfaces
+{
+    public class DBRepository
+    {
+        public void Guardar(string titulo, string contenido)
+        {
+            //Codigo
+        }
+
+        public string Leer(string titulo)
+        {
+            return "Ejemplo";
+        }
+
+    }
+}
